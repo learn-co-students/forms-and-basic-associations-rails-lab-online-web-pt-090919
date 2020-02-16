76112c3a67573440be70875fbf17668caf633617
jaredjames2020 <jaredjames2020@gmail.com>
2020-02-16 12:30:11 -0500

    add custom getter setter genre notes artist

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,34 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_id=(id)
+    self.genre = Genre.find(id)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def notes_attributes=(note_attrib)
+    note_attrib.each do |note|
+      if note.length > 0
+        note_new = Note.create(content: note)
+        self.notes << note_new
+      end
+    end
+  end
+
+  def notes_attributes
+    self.notes ? self.notes.all : nil
+  end
+
 end
